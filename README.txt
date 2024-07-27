57dba0a0d6d17c9dd445300cb4601a5fd17cbd04
Alexander N <sniknickitin@yandex.ru>
2024-07-28 01:53:00 +0300

    Minor setup/readme updates

--- README.txt
+++ README.txt
@@ -1,5 +1,13 @@
-**WORK IN PROGRESS**
+# What is this Library about?
+Easy-to-use (4 lines of code, actually) framework for training powerful predictive models!
 
-I opened this repo only for ITMO guys to watch it.
+# Description
+We made a mother-model, which consists of multiple layers of predictive models: ewma is used as trend, Prophet is used for getting seasonality, CatBoost is used for predicting residuals.
+Why did we do that? Because we needed a out-of-the-box solution, which could be used by non-ML users. 
 
-After that I will close this repo and finish the lib.
+# Maintained by
+Library is developed and being maintained by Teremok ML team
+
+# Contacts
+Our website: https://teremok.ru/
+ML team: you can contact us via telegram channel @pivo_txt
--- setup.py
+++ setup.py
@@ -11,14 +11,17 @@ classifiers = [
 setup(
   name='TeremokTSLib',
   version='0.0.1',
-  description='A very basic calculator',
+  description='Easy-to-use box ML solution for forcasting consumption',
+
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='https://t.me/pivo_txt',  
+  long_description_content_type="text/markdown",
+  url='https://teremok.ru/',  
   author='Alexander Nikitin',
   author_email='sniknickitin@yandex.ru',
   license='MIT', 
   classifiers=classifiers,
   keywords='forcasting', 
-  packages=find_packages(),
-  install_requires=[''] 
+  package_dir={"":"TeremokTSLib"},
+  packages=find_packages(where="TeremokTSLib"),
+  install_requires=["catboost >= 1.2.2", "pandas >= 2.1.4", "numpy >= 1.26.2", "prophet >= 1.1.5", "statsmodels >= 0.14.2", "scikit-learn >= 1.5.1"],
 )

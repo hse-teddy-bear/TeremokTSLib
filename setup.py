c53942fa5d1f55b67543600cc7959ec06b614731
Alexander N <sniknickitin@yandex.ru>
2024-09-11 13:34:58 +0300

    edited changelog

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ classifiers = [
  
 setup(
   name='TeremokTSLib',
-  version='1.2.2',
+  version='1.2.3',
   description='Easy-to-use box ML solution for forcasting consumption',
 
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
